7371c86c40d6ec375c2d17d9730176797d9aeafb
Axole Maranjana <axolemaranjana4@gmail.com>
2024-02-11 14:20:56 +0200

    Variable initialisation

---
+++ datastructures/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    double  price = 99.99;
+    float interest_rate = 3.67F; // Add suffix F
+    long file_size = 90000L; // Add suffix L
+    char letter = '1';
+    bool is_valid = false;
+    auto age = 21; // Automatically inferred
+
+    int number = {}; // Best practice
+
+    return 0;
+}
