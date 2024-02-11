24d71625b82f4a452df6f0f0550a95d0596c3898
Axole Maranjana <axolemaranjana4@gmail.com>
2024-02-11 14:39:12 +0200

    Random

--- datastructures/main.cpp
+++ datastructures/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include "cstdlib"
+#include "ctime"
+
+using namespace std;
 
 int main() {
     double  price = 99.99;
@@ -8,7 +12,19 @@ int main() {
     bool is_valid = false;
     auto age = 21; // Automatically inferred
 
-    int number = {}; // Best practice
+    int number = {}; // Best practice to use the brace initializer
+
+    srand(time(nullptr));
+    int random_number = rand() % 10;
+
+    // Exercise
+    const short min_value = 1;
+    const short max_value = 6;
+    int first_dice = (rand() % (max_value - min_value + 1)) + min_value;
+    int second_dice = (rand() % (max_value - min_value + 1)) + min_value;
+
+    cout << "First dice: " << first_dice << endl
+        << "Second dice: " << second_dice << endl;
 
     return 0;
 }
