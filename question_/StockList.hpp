3e9de0453bc8b3bae8059c517a0f812e94f55cfc
Axole Maranjana <axolemaranjana4@gmail.com>
2024-03-02 17:20:56 +0200

    Create header

---
+++ question_/StockList.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef StockList_hpp
+#define StockList_hpp
+
+#include"DVD.hpp"
+#include<string>
+
+using namespace std;
+
+class StockList
+{
+private:
+	string cCategoryList[30];
+	int cCategoryCounter;
+	DVD cDVDList[30];
+	int cDVDCounter;
+	DVD extractDVDFromString(string);
+
+public:
+	StockList();
+
+	void addDVD(DVD&);
+	bool addCategory(string);
+	void loadDVDsFromFile(string);
+	float determineAverageDVDPrice(string category);
+	int getListOfCategories(string categoryList[]);
+};
+
+#endif // !1
+
+
